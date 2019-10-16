d238099291fc623494ed7ea5ecdb4c5241fb3053
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-16 23:22:55 +0200

    Add -> Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import { Box } from "@chakra-ui/core";
 
+const Footer = () => {
+    return(
+        <Box bg="white" width="100%" height="65px" color="black" alignItems="center" overflow="hidden">
+            Footer
+        </Box>
+    )
+}
+
+export default Footer
