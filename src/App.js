c8b45d87030cbfaec94be09a103fca94fa0ebb9e
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-16 23:14:34 +0200

    Update -> Import & Implement SectionTwo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SectionOne from "./containers/sectionOne";
+import SectionTwo from "./containers/sectionTwo";
 import './styles/styles.css';
 
 function App() {
   return (
     <div>
       <SectionOne />
+      <SectionTwo />
     </div>
   );
 }
