fa28c427e65ba68970f838e9fd76276d427a9944
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-16 23:14:06 +0200

    Add -> SectionTwo

--- src/containers/sectionTwo.js
+++ src/containers/sectionTwo.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import { Box } from "@chakra-ui/core";
 
+const SectionTwo = () => {
+    return(
+        <Box bg="pink" width="100%" height="700px" color="white" overflow="hidden">
+            Section 02
+        </Box>
+    )
+}
+
+export default SectionTwo
