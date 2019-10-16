16b69c7fc83b807e3750cfe8c7b956b7fb38e51b
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-16 23:11:20 +0200

    Add -> SectionOne

--- src/containers/sectionOne.js
+++ src/containers/sectionOne.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import { Box } from "@chakra-ui/core";
 
+const SectionOne = () => {
+    return(
+        <Box bg="orange" width="100%" height="700px" color="white" overflow="hidden">
+            Section 01
+        </Box>
+    )
+}
+
+export default SectionOne
