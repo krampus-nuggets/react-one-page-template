286ebaa2b2c255de51487bee2470dfcb8a520347
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-16 23:16:30 +0200

    Add -> SectionThree

--- src/containers/sectionThree.js
+++ src/containers/sectionThree.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import { Box } from "@chakra-ui/core";
 
+const SectionThree = () => {
+    return(
+        <Box bg="lightblue" width="100%" height="700px" color="white"  overflow="hidden">
+            Section 03
+        </Box>
+    )
+}
+
+export default SectionThree
