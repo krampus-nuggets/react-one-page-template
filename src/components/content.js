e975ddaba01a62545b6d7db0c24a314a75bafdf4
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-10-17 03:02:48 +0200

    Add -> Content Component

--- src/components/content.js
+++ src/components/content.js
@@ -3,3 +3,17 @@ import { Box } from "@chakra-ui/core";
 import { Text } from "@chakra-ui/core";
 import "../styles/styles.css";
 
+const Content = (props) => {
+    return (
+        <Box bg="#ffffff" color="#000000" className="contentSection">
+            <Text fontSize="60px" m={0} textAlign="center">
+                { props.title }
+            </Text>
+            <Text fontSize="40px" m={0} textAlign="center">
+                { props.shortDesc }
+            </Text>
+        </Box>
+    )
+}
+
+export default Content
